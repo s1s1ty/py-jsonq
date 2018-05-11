d06baa3460be36fe7847ff3e47ae9630185d7f25
s1s1ty <shaonty.dutta@gmail.com>
2018-05-12 00:41:24 +0600

    where clause add

--- helper.py
+++ helper.py
@@ -2,48 +2,53 @@ class Helper(object):
     """docstring for Helper."""
     def __init__(self):
         self.condition_mapper = {
-            "=": 'is_equal'
+            '=': '_is_equal',
+            '!=': '_is_not_equal',
+            '>': '_is_greater',
+            '<': '_is_smaller',
+            '>=': '_is_greater_equal',
+            '<=': '_is_smaller_equal',
+            'in': '_is_in',
+            'notin': '_is_not_in',
+            'null': '_is_null',
+            'notnull': '_is_not_null',
+            'startswith': '_is_starts_with',
+            'endswith': '_is_ends_with',
         }
 
-    def is_equal(self, left_val, right_val):
+    def _is_equal(self, left_val, right_val):
         return left_val == right_val;
 
-    def is_not_equal(self, left_val, right_val):
+    def _is_not_equal(self, left_val, right_val):
         return left_val != right_val;
 
-    def is_greater(self, left_val, right_val):
+    def _is_greater(self, left_val, right_val):
         return left_val > right_val;
 
-
-    def is_smaller(self, left_val, right_val):
+    def _is_smaller(self, left_val, right_val):
         return left_val < right_val;
 
-
-    def is_greater_or_equal(self, left_val, right_val):
+    def _is_greater_equal(self, left_val, right_val):
         return left_val >= right_val;
 
-
-    def is_smaller_or_equal(self, left_val, right_val):
+    def _is_smaller_equal(self, left_val, right_val):
         return left_val <= right_val;
 
+    def _is_in(self, key, arr):
+        return isinstance(arr, list) and (key in arr)
 
-    def is_in(self, key, arr):
-        return Array.isArray(arr) and (key in arr)
+    def _is_not_in(self, key, arr):
+        return isinstance(arr, list) and (key not in arr)
 
+    def _is_null(self, val):
+        return val is None;
 
-    def is_not_in(self, key, arr):
-        return Array.isArray(arr) and (key not in arr)
+    def _is_not_null(self, val):
+        return val is not None;
 
-
-    def is_null(self, val):
-        return val == None;
-
-
-    def is_not_null(self, val):
-        return val != None;
-
-    def match(self, left_val, op, right_val):
+    def _match(self, left_val, op, right_val):
         if (op not in self.condition_mapper):
             raise ValueError('Invalid where condition given')
 
-        return self.condition_mapper[op](left_val, right_val)
+        func = getattr(self, self.condition_mapper.get(op))
+        return func(left_val, right_val)
--- query.py
+++ query.py
@@ -10,27 +10,29 @@ class JsonQuery(object):
 
     def __init__(self, file_path=""):
         """
-        * Set main json file path
-        * @param "" file_path
+        :@param file_path: Set main json file path
+        :@type file_path: string
         """
         if file_path != "":
             self.set_path(file_path)
 
-        self._reset_queries()
+        self.__reset_queries()
         self._helper = Helper()
 
-    def _reset_queries(self):
+    def __reset_queries(self):
         """
-        * complete me
+        :Reset previous query data
         """
         self._queries = [];
         self._current_query_index = 0;
 
-    def _parse_json_file(self, file_path):
+    def __parse_json_file(self, file_path):
         """
-        * Process Json file data
-        * @param file_path
-        * @throws FileNotFoundError
+        :Process Json file data
+        :@param file_path
+        :@type file_path: string
+
+        :@throws FileNotFoundError
         """
         if file_path == '' or os.path.splitext(file_path)[1] != '.json':
             raise FileNotFoundError('Json file not found')
@@ -40,57 +42,182 @@ class JsonQuery(object):
 
         self._json_data = copy.deepcopy(self._raw_data)
 
-    def _get_value(self, key, data):
+    def __get_value_from_data(self, key, data):
         """
-        * Find value from json data
-        * @pram key
-        * @pram data
-        * @return object value
-        * @throws KeyError
+        :Find value from json data
+        :@pram key
+        :@type: string
+
+        :@pram data
+        :@type data: dict
+
+        :@return object value
+        :@throws KeyError
         """
         if key not in data:
             raise KeyError("Key not exists")
-
-        return data[key]
+        return data.get(key)
 
     def get(self):
         """
-        * Getting prepared data
-        * @return object
+        :Getting prepared data
+        :@return object
         """
+        self.__prepare()
         return self._json_data
 
     def set_path(self, file_path):
         """
-        * Set main json file path
-        * @param file_path
-        * @throws FileNotFoundError
+        :Set main json file path
+        :@param file_path
+        :@type file_path: string
+
+        :@throws FileNotFoundError
         """
-        self._parse_json_file(file_path)
+        self.__parse_json_file(file_path)
         return self
 
     def at(self, root):
         """
-        * Set root where PyJsonq start to prepare
-        * @param root
-        * @return self
-        * @throws KeyError
+        :Set root where PyJsonq start to prepare
+        :@param root
+        :@type root: string
+
+        :@return self
+        :@throws KeyError
         """
         leafs = root.strip(" ").split('.')
 
         for leaf in leafs:
-            self._json_data = self._get_value(leaf, self._json_data)
-
+            self._json_data = self.__get_value_from_data(leaf, self._json_data)
         return self
+
+    def __store_query(self, query_items):
+        """
+        :make where clause
+        :@param query_items
+        :@type query_items: list
+        """
+        index = self._current_query_index
+        self._queries[index].append(query_items)
+
+    def __prepare(self):
+        """
+        :prepare query result
+        """
+        if len(self._queries) > 0:
+            self.__execute_queries()
+            self.__reset_queries()
+
+    def __execute_queries(self):
+        pass
 
     # ---------- Query Methods ------------- #
 
-    def where(self, key, operator, val):
+    def where(self, key, operator, value):
         """
-        * Apply where condition
-        * @param key
-        * @param operator
-        * @param val
-        * @return self
+        :make where clause
+        :@param key
+        :@param operator
+        :@param value
+        :@type key,operator,value: string
+
+        :@return self
         """
-        pass
+        self.__store_query({"key": key, "operator": operator, "value": value})
+        return self
+
+    def or_where(self, key, operator, value):
+        """
+        :make or_where clause
+        :@param key
+        :@param operator
+        :@param value
+        :@type key, operator, value: string
+
+        :@return self
+        """
+        self.where(key)
+        return self
+
+    def where_in(self, key, value):
+        """
+        :make where_in clause
+        :@param key
+        :@param value
+        :@type key, value: string
+
+        :@return self
+        """
+        self.where(key, 'in', value)
+        return self
+
+    def where_not_in(self, key, value):
+        """
+        :make where_not_in clause
+        :@param key
+        :@param value
+        :@type key, value: string
+
+        :@return self
+        """
+        self.where(key, 'in', value)
+        return self
+
+    def where_null(self, key):
+        """
+        :make where_null clause
+        :@param key
+        :@type key: string
+
+        :@return self
+        """
+        self.where(key, 'null')
+        return self
+
+    def where_not_null(self, key):
+        """
+        :make where_not_null clause
+        :@param key
+        :@type key: string
+
+        :@return self
+        """
+        self.where(key, 'notnull')
+        return self
+
+    def where_start_with(self, key, value):
+        """
+        :make where_start_with clause
+        :@param key
+        :@param value
+        :@type key,value: string
+
+        :@return self
+        """
+        self.where(key, 'startswith', value)
+        return self
+
+    def where_ends_with(self, key, value):
+        """
+        :make where_ends_with clause
+        :@param key
+        :@param value
+        :@type key,value: string
+
+        :@return self
+        """
+        self.where(key, 'endswith', value)
+        return self
+
+    def where_contains(self, key, value):
+        """
+        :make where_contains clause
+        :@param key
+        :@param value
+        :@type key,value: string
+
+        :@return self
+        """
+        self.where(key, 'contains', value)
+        return self
